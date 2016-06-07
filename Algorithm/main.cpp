4f6b12b9abee660c9f2ab4c66a35ec126ee4d5d6
scuiaa <cuishidong555@gmail.com>
2016-06-07 21:03:00 +0800

    Start to code quick sort

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "I will code quick sort today";
+    std::cout << "I will code quick sort today!\n";
     return 0;
 }
